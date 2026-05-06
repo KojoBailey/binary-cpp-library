ce9feb5ea8a3acd9403a4f04aad5242bc1923fdc
Kojo Bailey <kojomolojo@gmail.com>
2026-05-06 02:57:31 +0100

    Remove useless comments from type abbreviations

--- include/kojo/binary.hpp
+++ include/kojo/binary.hpp
@@ -17,37 +17,22 @@ namespace kojo { /* Kojo Bailey */
 
 namespace type_abbreviations {
 	using std::byte;
-	using u8  = std::uint8_t;       // 8-bit unsigned   (0 - 255)
-	using u16 = std::uint16_t;      // 16-bit unsigned  (0 - 65,535)
-	using u32 = std::uint32_t;      // 32-bit unsigned  (0 - 4,294,967,295)
-	using u64 = std::uint64_t;      // 64-bit unsigned  (0 - 18,446,744,073,709,551,615)
-	using i8  = std::int8_t;        // 8-bit signed     (-128 - 127)
-	using i16 = std::int16_t;       // 16-bit signed    (-32,768 - 32,767)
-	using i32 = std::int32_t;       // 32-bit signed    (-2,147,483,648 - 2,147,483,647)
-	using i64 = std::int64_t;       // 64-bit signed    (-9,223,372,036,854,775,808 - 9,223,372,036,854,775,807)
+	using u8  = std::uint8_t;
+	using u16 = std::uint16_t;
+	using u32 = std::uint32_t;
+	using u64 = std::uint64_t;
+	using i8  = std::int8_t;
+	using i16 = std::int16_t;
+	using i32 = std::int32_t;
+	using i64 = std::int64_t;
 	
-	using f16  = std::float16_t;    // 16-bit float
-	using bf16 = std::bfloat16_t;   // 16-bit brain float
-	using f32  = std::float32_t;    // 32-bit float
-	using f64  = std::float64_t;    // 64-bit float
-	using f128 = std::float128_t;   // 128-bit float
+	using f16  = std::float16_t;
+	using bf16 = std::bfloat16_t;
+	using f32  = std::float32_t;
+	using f64  = std::float64_t;
+	using f128 = std::float128_t;
 }
 
-// namespace util {
-//
-// template<std::integral T>
-// [[nodiscard]] constexpr T byteswap(const T value) noexcept
-// {
-// 	static_assert(std::has_unique_object_representations_v<T>, "T may not have padding bits");
-// 	std::uint8_t buffer[sizeof(T)];
-// 	std::memcpy(buffer, &value, sizeof(T));
-// 	std::ranges::reverse(buffer);
-// 	T result;
-// 	std::memcpy(&result, buffer, sizeof(T));
-// 	return result;
-// }
-//
-// }
 
 class Binary {
 public:
