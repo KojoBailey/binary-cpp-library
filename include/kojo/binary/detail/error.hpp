0e8ab482a98344bc7facc74091470a5e10079ba9
Kojo Bailey <kojomolojo@gmail.com>
2026-05-29 17:10:51 +0100

    Reorder stuff and fix phatnom error

--- include/kojo/binary/detail/error.hpp
+++ include/kojo/binary/detail/error.hpp
@@ -24,38 +24,45 @@ struct BinaryError {
 	struct InsufficientMemory {
 		static const std::uint32_t code = 1;
 
-		std::string to_string() const {
-			return std::format("Ran out of memory at {:08x} with requested size {}.",
-				reinterpret_cast<std::size_t>(address), size);
-		}
+		const std::byte* address;
+		const std::size_t size;
 
 		InsufficientMemory() = delete;
 		InsufficientMemory(const std::byte* _address, const std::size_t _size)
 			: address(_address), size(_size) {}
 
-		const std::byte* address;
-		const std::size_t size;
+		std::string to_string() const {
+			return std::format("Ran out of memory at {:08x} with requested size {}.",
+				reinterpret_cast<std::size_t>(address), size);
+		}
 	};
 
 	struct SizeExceeded {
 		static const std::uint32_t code = 2;
+
+		const std::size_t position;
+		const std::size_t size;
+
+		SizeExceeded() = delete;
+		SizeExceeded(const std::size_t _position, const std::size_t _size)
+			: position(_position), size(_size) {}
 
 		std::string to_string() const {
 			return std::format("Tried to access data at position {}, but data is only {} bytes.",
 				position, size
 			);
 		}
-
-		SizeExceeded() = delete;
-		SizeExceeded(const std::size_t _position, const std::size_t _size)
-			: position(_position), size(_size) {}
-
-		const std::size_t position;
-		const std::size_t size;
 	};
 
 	struct OutOfBounds {
 		static const std::uint32_t code = 3;
+
+		const std::byte* access_address;
+		const std::byte* end_address;
+
+		OutOfBounds() = delete;
+		OutOfBounds(const std::byte* _access_address, const std::byte* _end_address)
+			: access_address(_access_address), end_address(_end_address) {}
 
 		std::string to_string() const {
 			return std::format(
@@ -64,57 +71,50 @@ struct BinaryError {
 				reinterpret_cast<std::size_t>(end_address)
 			);
 		}
-
-		OutOfBounds() = delete;
-		OutOfBounds(const std::byte* _access_address, const std::byte* _end_address)
-			: access_address(_access_address), end_address(_end_address) {}
-
-		const std::byte* access_address;
-		const std::byte* end_address;
 	};
 
 	struct FileNotFound {
 		static const std::uint32_t code = 100;
 
-		std::string to_string() const {
-			return std::format("File at \"{}\" could not be found.", path.string());
-		}
+		const std::filesystem::path path;
 
 		FileNotFound() = delete;
 		FileNotFound(const std::filesystem::path _path)
 			: path(_path) {}
 
-		const std::filesystem::path path;
+		std::string to_string() const {
+			return std::format("File at \"{}\" could not be found.", path.string());
+		}
 	};
 
 	struct InvalidFile {
 		static const std::uint32_t code = 101;
+
+		const std::filesystem::path path;
+
+		InvalidFile() = delete;
+		InvalidFile(const std::filesystem::path _path)
+			: path(_path) {}
 
 		std::string to_string() const {
 			return std::format("File at \"{}\" is not a valid file. It may be a directory instead",
 				path.string()
 			);
 		}
-
-		InvalidFile() = delete;
-		InvalidFile(const std::filesystem::path _path)
-			: path(_path) {}
-
-		const std::filesystem::path path;
 	};
 
 	struct FileNotOpen {
 		static const std::uint32_t code = 102;
 
-		std::string to_string() const {
-			return std::format("Could not open file at \"{}\".", path.string());
-		}
+		const std::filesystem::path path;
 
 		FileNotOpen() = delete;
 		FileNotOpen(const std::filesystem::path _path)
 			: path(_path) {}
 
-		const std::filesystem::path path;
+		std::string to_string() const {
+			return std::format("Could not open file at \"{}\".", path.string());
+		}
 	};
 
 	std::variant<
